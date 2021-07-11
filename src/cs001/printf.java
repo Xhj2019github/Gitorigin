2f4d46be11cabedc6adfff0afe70ae135a436455
ywx <xu_hongjie@foxmail.com>
2021-07-11 21:17:31 +0800

    hot-fix 02

--- src/cs001/printf.java
+++ src/cs001/printf.java
@@ -12,8 +12,8 @@ public class printf {
         System.out.printf("1111111111111111111111111111");
         System.out.printf("1111111111111111111111111111");
         System.out.printf("1111111111111111111111111111");
-        System.out.printf("1111111111111111111111111111");
-        System.out.printf("1111111111111111111111111111");
+        System.out.printf("33333333333333333333333333333333333");
+        System.out.printf("22222222222222222222222222");
         System.out.println("hot-fix add ");
         System.out.println("hot-fix add ");
     }
