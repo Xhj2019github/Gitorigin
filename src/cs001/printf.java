a9753f6592b08374b7711be8867d6fbf56c34d62
ywx <xu_hongjie@foxmail.com>
2021-07-11 20:55:55 +0800

    commit 02

--- src/cs001/printf.java
+++ src/cs001/printf.java
@@ -3,5 +3,10 @@ package cs001;
 public class printf {
     public static void main(String[] args) {
         System.out.printf("asds");
+        System.out.printf("asds");
+        System.out.printf("asds");
+        System.out.printf("asds");
+        System.out.printf("asds");
+        System.out.printf("asds");
     }
 }
